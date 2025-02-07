00fd40dd96e5e45eec9a567b077efd9e5d4ad906
Михаил <misha.skopenko@yandex.ru>
2025-02-07 20:22:51 +0300

    add swagger property for dto

--- src/gameEvents/dto/gameEvents.dto.ts
+++ src/gameEvents/dto/gameEvents.dto.ts
@@ -1,31 +1,38 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import {
   IsString,
   IsOptional,
-  IsArray,
-  IsNumber,
   IsDateString,
+  IsNumber,
+  IsArray,
 } from 'class-validator';
 
 export class CreateEventDto {
+  @ApiProperty({ example: 'Board Game Night' })
   @IsString()
   title: string;
 
+  @ApiPropertyOptional({ example: 'Играем в настолки' })
   @IsOptional()
   @IsString()
   description?: string;
 
-  @IsOptional()
-  @IsString()
-  location?: string;
-
+  @ApiProperty({ example: '2025-02-05T18:30:00.000Z' })
   @IsDateString()
   eventDate: string;
 
+  @ApiProperty({ example: 5 })
   @IsNumber()
   count: number;
 
+  @ApiPropertyOptional({
+    example: [
+      { name: 'Иван', id: 'abc123' },
+      { name: 'Мария', id: 'abc345' },
+    ],
+  })
+  @IsOptional()
   @IsArray()
   @IsString({ each: true })
-  @IsOptional()
-  participants?: string[];
+  participants?: { name: string; id?: string }[];
 }
